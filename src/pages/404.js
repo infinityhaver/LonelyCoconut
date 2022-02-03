f0bf59b845142cd2773a34a8de34bb3e692d663e
Nathanael Gray <ngray@forefrontweb.com>
2022-02-02 21:51:03 -0500

    just working on stuff

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ const NotFoundPage = ({ data }) => {
   return (
     <>
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>You just hit a route that doesn&#39;t exist... the sadness. Shhhh.</p>
     </>
   )
 }
