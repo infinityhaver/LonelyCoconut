a5bc4e3247617afbe4f09b8cc907dda20a868946
Nathanael Gray <ngray@forefrontweb.com>
2022-02-02 23:02:07 -0500

    more stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,13 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: process.env.WPGRAPHQL_URL || `http://gatsby.local/graphql`,
+        // the only required plugin option for WordPress is the GraphQL url.
+        url:
+          process.env.WPGRAPHQL_URL ||
+          `http://gatsby.local/graphql`,
+          develop: {
+            hardCacheMediaFiles: true,
+        },
       },
     },
     `gatsby-transformer-sharp`,
@@ -12,6 +18,13 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-fontawesome-css`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/content/assets`,
+      },
+    },
+    {
       resolve: "gatsby-source-graphql",
       options: {
         typeName: "WPGraphQL",
@@ -19,5 +32,17 @@ module.exports = {
         url: `http://gatsby.local/graphql`,
       },
     },
+    {
+      resolve: 'gatsby-source-gravityforms',
+      options: {
+          // Base URL needs to include protocol (http/https)
+          baseUrl: 'http://gatsby.local',
+          // Gravity Forms API
+          api: {
+              key: 'ck_1f866e1a70fa79995817fffcb5a17f00de7cfe57',
+              secret: 'cs_e9772376c8121d85b5434ca9b4e5a5bb784d3cab',
+          },
+      },
+  },
   ],
 }
